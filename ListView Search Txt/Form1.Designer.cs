20a1449ab697fbcfae0133aea6ab80e041ca4465
Oclemy <oclemmi@gmail.com>
2017-03-22 16:48:01 -0700

    Initial Commit

---
+++ ListView Search Txt/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ListView_Search_Txt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mListView = new MetroFramework.Controls.MetroListView();
+            this.searchTxt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // mListView
+            // 
+            this.mListView.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.mListView.FullRowSelect = true;
+            this.mListView.Location = new System.Drawing.Point(57, 165);
+            this.mListView.Name = "mListView";
+            this.mListView.OwnerDraw = true;
+            this.mListView.Size = new System.Drawing.Size(348, 378);
+            this.mListView.TabIndex = 0;
+            this.mListView.UseCompatibleStateImageBehavior = false;
+            this.mListView.UseSelectable = true;
+            // 
+            // searchTxt
+            // 
+            // 
+            // 
+            // 
+            this.searchTxt.CustomButton.Image = null;
+            this.searchTxt.CustomButton.Location = new System.Drawing.Point(151, 1);
+            this.searchTxt.CustomButton.Name = "";
+            this.searchTxt.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.searchTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.searchTxt.CustomButton.TabIndex = 1;
+            this.searchTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.searchTxt.CustomButton.UseSelectable = true;
+            this.searchTxt.CustomButton.Visible = false;
+            this.searchTxt.Lines = new string[0];
+            this.searchTxt.Location = new System.Drawing.Point(146, 106);
+            this.searchTxt.MaxLength = 32767;
+            this.searchTxt.Name = "searchTxt";
+            this.searchTxt.PasswordChar = '\0';
+            this.searchTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.searchTxt.SelectedText = "";
+            this.searchTxt.SelectionLength = 0;
+            this.searchTxt.SelectionStart = 0;
+            this.searchTxt.ShortcutsEnabled = true;
+            this.searchTxt.ShowClearButton = true;
+            this.searchTxt.Size = new System.Drawing.Size(173, 23);
+            this.searchTxt.TabIndex = 1;
+            this.searchTxt.UseSelectable = true;
+            this.searchTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.searchTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.searchTxt.TextChanged += new System.EventHandler(this.searchTxt_TextChanged);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(57, 106);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(38, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Filter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 585);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.searchTxt);
+            this.Controls.Add(this.mListView);
+            this.Name = "Form1";
+            this.Text = "ListView Search Filter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroListView mListView;
+        private MetroFramework.Controls.MetroTextBox searchTxt;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ ListView Search Txt/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace ListView_Search_Txt
+{
+    public partial class Form1 : MetroForm
+    {
+        private List<Spacecraft> spacecrafts;
+        public Form1()
+        {
+            InitializeComponent();
+
+            setupListView();
+        }
+        /*
+         * SETUP LISTVIEW COLUMNS AND PRPERTIES
+         */
+        private void setupListView()
+        {
+            mListView.View = View.Details;
+            mListView.FullRowSelect = true;
+            mListView.Columns.Add("Name", 150);
+            mListView.Columns.Add("Destination", 230);
+
+            //LISTVIEW
+            spacecrafts = new List<Spacecraft>
+            {
+                new Spacecraft {Name="Casini",Destination = "Saturn"},
+                new Spacecraft {Name="Enterprise",Destination="Andromeda"},
+                new Spacecraft {Name="Challenger",Destination="Pluto"},
+                new Spacecraft {Name="New Horizon",Destination="Asteroid Belt"},
+                new Spacecraft {Name="Opportunity",Destination="Mars"},
+                new Spacecraft {Name="Curiosity",Destination="Mars"},
+                new Spacecraft {Name="Juno",Destination="Jupiter"},
+                new Spacecraft {Name="Apollo 15",Destination="Monn"},
+                new Spacecraft {Name="Apollo 17",Destination="Moon"},
+                new Spacecraft {Name="Voyager A",Destination="Alpha Centauri"},
+                new Spacecraft {Name="Voyager B",Destination="Proxima Centauri"},
+                new Spacecraft {Name="Rosetter",Destination="Venus"},
+                new Spacecraft {Name="Spitzer",Destination="Uranus"},
+                new Spacecraft {Name="Chandra",Destination="Mercury"},
+                 new Spacecraft{Name="Pioneer",Destination="Mars"},
+                new Spacecraft {Name="Atlantis",Destination="Jupiter"}
+            };
+
+            //POPULATE SPACECRAFTS
+            foreach (Spacecraft s in spacecrafts)
+            {
+                mListView.Items.Add(new ListViewItem(new []{s.Name,s.Destination}));
+            }
+           
+        }
+
+        /*
+         * SEARCH DATA
+         */
+        private void searchData(string searchTerm)
+        {
+                mListView.Items.Clear();
+           
+            
+                foreach (Spacecraft s in spacecrafts)
+                {
+                    if (s.Name.ToLower().Contains(searchTerm.ToLower()) || s.Destination.ToLower().Contains(searchTerm.ToLower()))
+                    {
+                        mListView.Items.Add(new ListViewItem(new[] { s.Name, s.Destination }));
+
+                    }
+                }
+            
+           
+        }
+
+        /*
+         * SEARCH VALUE CHANGED
+         */
+        private void searchTxt_TextChanged(object sender, EventArgs e)
+        {
+            searchData(searchTxt.Text);
+        }
+    }
+
+    /*
+     * POCO
+     */
+    class Spacecraft
+    {
+        public string Name { get; set; }
+
+        public string Destination { get; set; }
+    }
+}
